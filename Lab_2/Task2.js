e735aec57f7bda17fc6c55c1b2c1e1e9dc97e113
Olha Sydoruk <130238768+OlhaSydoruk@users.noreply.github.com>
2023-04-09 13:48:06 +0200

    Add files via upload
    rewrote the 2nd task

--- Lab_2/Task2.js
+++ Lab_2/Task2.js
@@ -1,21 +1,8 @@
-//Напишіть функцію, яка бере два рядки і повертає true, якщо вони є анаграмами одне одного.
-function toMass(string) {
-    let listChar = [];
-    for (let char of string) {
-        listChar.push(char)
-    }
-    return listChar;
+
+function anagrama(a,b){
+const sortA = a.split('').sort().join('');
+const sortB = b.split('').sort().join('');
+return sortA===sortB;
 }
-
-
-let anagrama = function (a, b) {
-    let listB = toMass(b);
-    let listA = toMass(a);
-    let flag = true;
-    listB.forEach((i) => {
-        listA.includes(i) ? listA = listA.filter(el => el !== i) : flag = false
-    });
-    return flag;
-}
-
-console.log("It`s anagrama ?   " + anagrama('stre', 'ster'))
+console.log(anagrama("Stress","strSes"));//true
+console.log(anagrama("Stress","rteSssss"));//false
