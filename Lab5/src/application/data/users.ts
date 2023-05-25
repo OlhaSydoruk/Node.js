e526489fd21cf9498892227ee58a9cfd22d97487
Olha Sydoruk <130238768+OlhaSydoruk@users.noreply.github.com>
2023-05-25 23:24:53 +0200

    Delete Lab5 directory

--- Lab5/src/application/data/users.ts
+++
@@ -1,6 +0,0 @@
-import{IUser} from "../models/user";
-export const users: Array<IUser>=[
-    {id:'1Lipton', name: 'Alan', surname: 'Lipton', age:27},
-    {id:'2Lavazza',name: 'Linda', surname: 'Lavazza', age:22 },
-    {id:'3Fox', name:'Fox', surname:'Lays', age:36}
-]
