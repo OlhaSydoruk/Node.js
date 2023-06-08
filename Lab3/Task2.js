85b998842269239a88cc497b680f0663f305b458
Olha Sydoruk <sidorukolgasev@gmail.com>
2023-06-08 20:05:56 +0200

    lab8

--- Lab3/Task2.js
+++
@@ -1,7 +0,0 @@
-function anagrama(a,b){
-    const sortA = a.split('').sort().join('');
-    const sortB = b.split('').sort().join('');
-    return sortA===sortB;
-}
-console.log(anagrama("Stress","strSes"));//true
-console.log(anagrama("Stress","rteSssss"));//false
